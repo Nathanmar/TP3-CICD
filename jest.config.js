d43b807b3d9045b71c2c29f80928005b6474ac70
Nathan <nathan.martel.edu@gmail.com>
2026-04-09 15:09:48 +0200

    test: ajout des tests E2E et configuration des seuils de couverture pour la CI

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,16 @@ export default {
             },
         ],
     },
+    testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
+    collectCoverage: true,
+    coverageReporters: ['text', 'text-summary'],
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70,
+        },
+    },
 };
